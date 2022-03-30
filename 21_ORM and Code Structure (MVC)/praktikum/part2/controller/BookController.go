5236bd79d958932271dc3c8e60b2055b68f77b51
priyanfadhil <priyanfadhil@gmail.com>
2022-03-30 17:55:06 +0700

    merevisi section 12

---
+++ 21_ORM and Code Structure (MVC)/praktikum/part2/controller/BookController.go
@@ -0,0 +1,99 @@
+package controller
+
+import (
+	"net/http"
+	"part2/config"
+	"part2/model"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+// get all books
+func GetBooksController(c echo.Context) error {
+	var books []model.Book
+
+	if err := config.DB.Find(&books).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success get all books",
+		"books":   books,
+	})
+}
+
+// get book by id
+func GetBookController(c echo.Context) error {
+	// your solution here
+	book := model.Book{}
+
+	id, _ := strconv.Atoi(c.Param("id"))
+	if err := config.DB.Find(&book, id).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	if book.ID == 0 {
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"messages": "invalid id",
+		})
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"messages": "success get book",
+		"book":     book,
+	})
+}
+
+// create new book
+func CreateBookController(c echo.Context) error {
+	book := model.Book{}
+	c.Bind(&book)
+
+	if err := config.DB.Save(&book).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success create new book",
+		"book":    book,
+	})
+}
+
+// delete book by id
+func DeleteBookController(c echo.Context) error {
+	// your solution here
+	book := model.Book{}
+	id, _ := strconv.Atoi(c.Param("id"))
+	if err := config.DB.Delete(&book, id).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	if book.ID == 0 {
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"messages": "invalid id",
+		})
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success delete book",
+		"book":    book,
+	})
+}
+
+// update book by id
+func UpdateBookController(c echo.Context) error {
+	// your solution here
+	book := model.Book{}
+	id, _ := strconv.Atoi(c.Param("id"))
+	if err := config.DB.First(&book, id).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	if book.ID == 0 {
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"messages": "invalid id",
+		})
+	}
+	c.Bind(&book)
+	if err := config.DB.Save(&book).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success update book",
+		"book":    book,
+	})
+}
